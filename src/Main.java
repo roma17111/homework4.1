c2dfc5eaa2e37f5a853f233cd4b4cc66616df77a
Роман <romanze1706@gmail.com>
2022-11-03 15:39:44 +0300

    Задание 3.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int age = 36;
+        boolean baby = age >= 2 && age <= 6;
+        boolean pyupl = age >=7 && age <= 18;
+        boolean student = age  >18 && age < 24;
+        if (baby) {System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");}
+        else if (pyupl) {System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");}
+        else if (student) {System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет");}
+        else {System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");}
+
     }
 }
